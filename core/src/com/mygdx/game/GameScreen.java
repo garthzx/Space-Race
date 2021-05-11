2dad5f34a87928e3af49b6492e51368780007e5f
Garth <gotfangarth@gmail.com>
2021-05-11 16:56:22 +0800

    Completed Circles

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -23,8 +25,13 @@ public class GameScreen extends ScreenAdapter {
     private SpaceRaceGame spaceRaceGame;
     private SpriteBatch batch;
 
-    private Circle circle;
+    private Array<Circle> circleArrayToLeft;
+    private Array<Circle> circleArrayToRight;
 
+    private static final float CIRCLE_RADIUS = 4;
+
+    private float timer = 0;
+    private static final float CIRCLE_SPEED = 75;
     public GameScreen(SpaceRaceGame spaceRaceGame) {
         this.spaceRaceGame = spaceRaceGame;
     }
@@ -39,10 +46,18 @@ public class GameScreen extends ScreenAdapter {
         shapeRenderer = new ShapeRenderer();
         batch = new SpriteBatch();
 
-        circle = new Circle(50, 50, 4);
+        circleArrayToLeft = new Array<>();
+        circleArrayToRight = new Array<>();
+        for (int i = 0; i < 25; i++) {
+            createCirclesToRight();
+        }
+        for (int i = 0; i < 25; i++) {
+            createCirclesToLeft();
+        }
     }
     @Override
     public void render(float delta) {
+
         clearScreen();
 
         draw();
@@ -58,13 +73,27 @@ public class GameScreen extends ScreenAdapter {
         shapeRenderer.setColor(Color.WHITE);
 //        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.circle(circle.x, circle.y, circle.radius);
+        drawDebugAllCircles();
 
         shapeRenderer.end();
     }
-
-    private void drawAllCircles() {
-        // init empty
+    private void createCirclesToRight() {
+        float randX = MathUtils.random(WORLD_WIDTH);
+        float randY = MathUtils.random(60, WORLD_HEIGHT);
+        circleArrayToLeft.add(new Circle(randX, randY, CIRCLE_RADIUS));
+    }
+    private void createCirclesToLeft() {
+        float randX = MathUtils.random(WORLD_WIDTH);
+        float randY = MathUtils.random(60, WORLD_HEIGHT);
+        circleArrayToRight.add(new Circle(randX, randY, CIRCLE_RADIUS));
+    }
+    private void drawDebugAllCircles() {
+        for (Circle c : circleArrayToLeft) {
+            shapeRenderer.circle(c.x, c.y, c.radius);
+        }
+        for (Circle c : circleArrayToRight) {
+            shapeRenderer.circle(c.x, c.y, c.radius);
+        }
     }
     private void clearScreen() {
         Gdx.gl.glClearColor(Color.valueOf("#2b2e3b").r, Color.valueOf("#2b2e3b").g,
@@ -73,7 +102,27 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void update(float dt) {
+        timer += dt;
+        // move circles
+        for (Circle c : circleArrayToLeft) {
+            c.x = c.x  - (CIRCLE_SPEED * dt);
 
+            if (c.x + c.radius < 0) {
+                float randY = MathUtils.random(60, WORLD_HEIGHT);
+                c.x = WORLD_WIDTH + c.radius;
+                c.y = randY;
+            }
+        }
+
+        for (Circle c : circleArrayToRight) {
+            c.x = c.x + (CIRCLE_SPEED * dt);
+
+            if (c.x + c.radius > WORLD_WIDTH) {
+                c.x = 0 - c.radius;
+                float randY = MathUtils.random(60, WORLD_HEIGHT);
+                c.y = randY;
+            }
+        }
     }
 
     private void draw() {
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.forceExit = false;
-		config.width = 640;
+		config.width = 480;
 		config.height = 480;
 		new LwjglApplication(new SpaceRaceGame(), config);
 	}
